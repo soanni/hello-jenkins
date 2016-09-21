7588198d08950abcc331917a2a82341565bf5282
Andrey Solodov <andrey.solodov@aurea.com>
2016-09-21 09:22:37 -0400

    test jenkins2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello codenvy');
+  res.send('hello Codenvy');
 });
  
 app.listen(process.env.PORT || 5001);
